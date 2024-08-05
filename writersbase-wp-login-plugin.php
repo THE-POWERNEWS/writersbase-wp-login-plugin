528accbdb4cae149489a95a29a9ba791cc3137cc
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2024-08-05 19:51:43 +0900

    rename

--- writersbase-wp-login-plugin.php
+++ writersbase-wp-login-plugin.php
@@ -1,7 +1,7 @@
 <?php
 /*
-  Plugin Name: wb-login
-  Plugin URI:
+  Plugin Name: writersbase-wp-login-plugin
+  Plugin URI: https://github.com/THE-POWERNEWS/writersbase-wp-login-plugin
   Description: ログイン処理をオーバライド
   Version: 1.0.0
   Author: Tatsuya Koishi
