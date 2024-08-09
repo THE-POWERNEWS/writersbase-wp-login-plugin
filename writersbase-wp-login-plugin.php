2c3b813da52c66c367fd2d3bfef888b8dca73afd
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2024-08-09 15:58:55 +0900

    update wp_check_password

--- writersbase-wp-login-plugin.php
+++ writersbase-wp-login-plugin.php
@@ -7,5 +7,23 @@
   Author: Tatsuya Koishi
   Author URI: https://github.com/pooza
  */
+require_once ABSPATH . '/wp-includes/class-phpass.php';
 
+if (!function_exists('wp_hash_password')) {
+  function wp_hash_password($password) {
+    return password_hash($password);
+  }
+}
 
+if (!function_exists('wp_check_password')) {
+  function wp_check_password($password, $hash, $uid = '') {
+  	$hasher = new PasswordHash( 8, true );
+	  return apply_filters(
+      'check_password',
+      password_verify($password, $hash) || $hasher->CheckPassword($password, $hash),
+      $password,
+      $hash,
+      $uid
+    );
+  }
+}
